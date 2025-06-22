7ac9dd22d8ae248ffd02f707a6e34b9d4f23794e
Roshan <155551955+okroshan4u@users.noreply.github.com>
2025-06-22 22:35:14 +0530

    Uploaded the js file

---
+++ dictionary/script.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById("btn")
+const input = document.getElementById("input")
+const container = document.querySelector(".container")
+
+btn.addEventListener("click",async()=>{
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${input.value}`)
+
+    const data = await response.json();
+
+    console.log(data)
+    // console.log(data[0])
+    // console.log(data[0].word, data[0].phonetics , data[0].phonetics[0].audio , data[0].phonetics[1].text)
+    console.log(data[0].meanings[0].definitions)
+    console.log(data[0].meanings[0].definitions[0].definition)
+
+    container.innerHTML = data[0].meanings[0].definitions[0].definition
+})
