be5108b4deccad5836060796dcc36cf8f832ff8c
Roshan <155551955+okroshan4u@users.noreply.github.com>
2025-06-23 13:02:49 +0530

    Update script.js

--- dictionary/script.js
+++ dictionary/script.js
@@ -8,7 +8,7 @@ btn.addEventListener("click",async()=>{
     const data = await response.json();
 
    
-    // console.log(data[0].word, data[0].phonetics , data[0].phonetics[0].audio , data[0].phonetics[1].text)
+   
     console.log(data[0].meanings[0].definitions)
     console.log(data[0].meanings[0].definitions[0].definition)
 
